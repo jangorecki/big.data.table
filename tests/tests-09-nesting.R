a23b7f3f0374b005cff0f141eab189872d0fea89
jangorecki <j.gorecki@wit.edu.pl>
2016-02-12 21:58:06 +0000

    unit tests to verify nesting sessions, cascade nested nodes with big.data.table possible

---
+++ tests/tests-09-nesting.R
@@ -0,0 +1,39 @@
+# wrap nodes into big.data.table, then expose it as gateway node so Rserve clients can consume big.data.table interface
+
+library(Rserve)
+library(data.table)
+library(big.data.table)
+
+# initialize R big.data.table gateways
+invisible(Rserve(debug = FALSE, port = 23311L, args = c("--no-save")))
+invisible(Rserve(debug = FALSE, port = 23312L, args = c("--no-save")))
+rsc = rscl.connect(port = c(23311:23312))
+rscl.require(rsc, package = c("data.table","big.data.table"))
+
+# rsc - big.data.table gateway nested in length 1L list()
+# rscl - list of nodes
+qsetup = quote({
+    rscl = rscl.connect(port = 33311:33314)
+    rscl.require(rscl, package = "data.table")
+    data = quote(as.data.table(iris))
+    bdt = as.big.data.table(data, rscl)
+    TRUE
+})
+rscl.eval(rsc, qsetup, lazy = FALSE)
+
+rvar = rscl.eval(rsc, rscl.ls(rscl))
+r = rbindlist(rscl.eval(rsc, bdt[, lapply(.SD, mean), Species], simplify = FALSE))
+
+stopifnot(
+    all(rvar=="x"),
+    is.data.table(r), ncol(r)==5L, nrow(r)==24L
+)
+
+# shutdown gateway
+
+rscl.close(rsc)
+rsc = RSclient::RSconnect(port = 23311)
+RSclient::RSshutdown(rsc)
+rsc = RSclient::RSconnect(port = 23312)
+RSclient::RSshutdown(rsc)
+rm(rsc)
